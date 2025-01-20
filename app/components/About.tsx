ec3d8a8d680eda23f7f6b2a3e8728f48ac6affee
Ally Elvis Nzeyimana <allyelvis6569@gmail.com>
2025-01-20 18:27:50 +0200

    Create About.tsx

---
+++ app/components/About.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <section id="about" className="mb-12">
+      <h2 className="text-2xl font-bold mb-4">About Us</h2>
+      <p>
+        Aenzbi is a leading software and internet company dedicated to developing innovative software solutions and
+        providing exceptional internet services. Our team of experienced professionals is committed to delivering
+        high-quality products and services that meet the needs of our clients.
+      </p>
+    </section>
+  )
+}
+
